af4500c8a892554b0ce41c1c63ada4b26179589c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-24 21:55:26 -0400

    handle unregistered

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,20 @@
 /// <reference types="cypress" />
 after(() => {
   // grab the server code coverage only once after all tests
-  cy.request('/__coverage__', { log: false })
-    .then(r => Cypress._.get(r, 'body.coverage'), { log: false })
+  cy.request({
+    url: '/__coverage__',
+    log: false,
+    failOnStatusCode: false
+  })
+    .then(r => {
+      return Cypress._.get(r, 'body.coverage', null)
+    }, { log: false })
     .then(coverage => {
+      if (!coverage) {
+        // we did not get code coverage - this is the
+        // original failed request
+        return
+      }
       cy.task('combineCoverage', coverage)
     })
 })
